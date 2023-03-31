77f9f01af3fb32de204c4291ecc0de7aa8cd222d
Antoine Bon <antoine.bon@gmail.com>
2023-03-31 22:33:59 +0200

    extend __init__

--- src/cosmokit/__init__.py
+++ src/cosmokit/__init__.py
@@ -1,6 +1,6 @@
 from .adapters.repository import AbstractRepository, MemmoryRepository
 from .domain.messages import Command, Event
-from .domain.model import Aggregate, Entity
+from .domain.model import Aggregate, Entity, ValueObject
 from .service_layer.messagebus import MessageBus
 from .service_layer.unit_of_work import AbstractUnitOfWork, MemmoryUnitOfWork
 
@@ -11,6 +11,7 @@ __all__ = [
     "AbstractUnitOfWork",
     "MemmoryUnitOfWork",
     "Entity",
+    "ValueObject",
     "Event",
     "Command",
     "MessageBus",
